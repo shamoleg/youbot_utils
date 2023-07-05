88d2fa6a135814e9f97d14584f9d93d7d345c2a4
Askenavn <askenavn@gmail.com>
2023-07-05 21:09:33 +0700

    P-controller

--- PID.py
+++ PID.py
@@ -1,77 +1,75 @@
 #!/usr/bin/env python
 from geometry_msgs.msg import PointStamped, Twist
 import rospy
+import numpy as np
+from scipy import integrate
+
+
+
+class PID:
+    def __init__(self, input_sig, back_sig = None, Ki=1, Kp = 1, Kd =1):
+        self.input_sig = input_sig
+        self.Ki = Ki
+        self.Kp = Kp
+        self.Kd = Kd
+        self.back_sig = back_sig
+        self.output_sig = None
+
+    def func(self, put):
+        return self.Kp*put
+    
+    def getBack_sig(self):
+        #self.back_sig = чему-то
+        pass
+
+    def calculate(self):
+        output_sig = self.func(self.input_sig)
+        if self.back_sig is not None:
+            output_sig = self.input_sig - self.back_sig
+        else:
+            self.output_sig = output_sig
+            return 
+        self.output_sig = output_sig
+        return output_sig
 
 
 class Youbot:
     def __init__(self, robotID: str):
         self.robotID = robotID
-        self.cur_x = None
-        self.cur_y = None
-        self.cur_z = None
-        self.updatepose()
-        self.velPub = rospy.Publisher('/base/cmd_vel', Twist, queue_size=1)
+        self.cur_pose = PointStamped()
+        self.pub_cmd_vel = rospy.Publisher('/base/cmd_vel', Twist, queue_size=1)
+        rospy.Subscriber('/marker_pose', PointStamped, self.callback_pose)
 
-    def _callbackpose(self, data: PointStamped):
-        self.cur_x = round(data.point.x, 2)
-        self.cur_y = round(data.point.y, 2)
-        self.cur_z = round(data.point.z, 2)
-               
-    def updatepose(self):
-        rospy.init_node('listener', anonymous=True)
-        rospy.Subscriber('/marker_pose', PointStamped, self._callbackpose)
-        rospy.sleep(0.05)
+    def callback_pose(self, data: PointStamped):
+        self.cur_pose = data
 
-    def goTo(self, x, y):
-        self.updatepose()
+    def setGoal(self, goal_x, goal_y, threshold=0.005):
         vel = Twist()
-        while round(x, 2) != self.cur_x:
-            if x > self.cur_x:
-                coef = 1
-            elif x < self.cur_x:
-                coef = -1
-            else:
-                coef = 0
-            vel.linear.x = 0.1*coef
-            self.velPub.publish(vel)
-            self.updatepose()
-        vel = Twist()
-        self.velPub.publish(vel)
+        coef_P = 0.1
+        error_x = goal_x - self.cur_pose.point.x
+        while abs(error_x) > abs(threshold):
+            vel.linear.x = coef_P * error_x
+            error_x = goal_x - self.cur_pose.point.x
+            self.pub_cmd_vel.publish(vel)
 
-    def setGoal(self, x, y, treshold=0.005):
-        try:
-            vel = Twist()
-            self.updatepose()
-            start_x = self.cur_x
-            start_y = self.cur_y
-            t1 = rospy.Time.now()
-            while True:
-                if x > 0:
-                    if (abs(self.cur_x) - abs(start_x) < x+treshold) and (abs(self.cur_x) - abs(start_x) > x-treshold):
-                        break
-                    coef = 1
-                elif x < 0:
-                    if (abs(start_x) - abs(self.cur_x) < abs(x)+treshold) and (abs(start_x) - abs(self.cur_x)  > abs(x)-treshold):
-                        break
-                    coef = -1
-                else:
-                    coef = 0
-                    break
-                vel.linear.x = 0.05*coef
-                self.velPub.publish(vel)
-                self.updatepose()
-                print('cur_x: ' + str(self.cur_x))
-        finally:
-            vel = Twist()
-            self.velPub.publish(vel)
-            print(self.cur_x)
+        self.pub_cmd_vel.publish(Twist())
 
 
+if __name__ == "__main__":
+    # u = Youbot('\x01')
+    # u.setGoal(0.1, 0)
+    # u.setGoal(0.1, 0)
+    # u.setGoal(-0.1, 0)
+    # u.setGoal(0.1, 0)
+    # u.setGoal(-0.1, 0)
 
+    p = PID(2, Kp=0.8) 
+    p.calculate()
+    print(p.output_sig)
+    p.back_sig = p.output_sig*1.25
+    p.calculate()
+    print(p.output_sig)
+    p.back_sig = p.output_sig*1.25
+    p.calculate()
+    print(p.output_sig)
 
-u =Youbot('\x01')
-u.setGoal(0.1, 0)
-u.setGoal(0.1, 0)
-u.setGoal(-0.1, 0)
-u.setGoal(0.1, 0)
-u.setGoal(-0.1, 0)
