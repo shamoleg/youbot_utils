a7413f4256c8dd60e65a7a6c77a2eb8b6bad9ef7
Askenavn <askenavn@gmail.com>
2023-07-06 21:39:40 +0700

    removed youbot class

--- PID.py
+++ PID.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python
-from geometry_msgs.msg import PointStamped, Twist
-import rospy
 from time import time
 
 class PID:
@@ -74,32 +72,6 @@ class PID:
     #         self.cur_error = error_sig
     #     return self.updatePID()
 
-
-class Youbot:
-    def __init__(self, robotID: str):
-        self.robotID = robotID
-        self.cur_pose = PointStamped()
-        self.pub_cmd_vel = rospy.Publisher('/base/cmd_vel', Twist, queue_size=1)
-        rospy.Subscriber('/marker_pose', PointStamped, self.callback_pose)
-        self.pid = PID()
-
-    def callback_pose(self, data: PointStamped):
-        self.cur_pose = data
-
-    def setGoal(self, goal_x, goal_y, threshold=0.005):
-        vel = Twist()
-        coef_P = 0.1
-        error_x = goal_x - self.cur_pose.point.x
-        while abs(error_x) > abs(threshold):
-            vel.linear.x = coef_P * error_x
-            error_x = goal_x - self.cur_pose.point.x
-            self.pub_cmd_vel.publish(vel)
-
-        self.pub_cmd_vel.publish(Twist())
-
-    def setGoalwithPID(self, goal_X, goal_y):
-        vel = Twist()
-
 def test_func(inp):
     output = 0.0
     alpha = 0.02
@@ -107,12 +79,6 @@ def test_func(inp):
     return output
 
 if __name__ == "__main__":
-    # u = Youbot('\x01')
-    # u.setGoal(0.1, 0)
-    # u.setGoal(0.1, 0)
-    # u.setGoal(-0.1, 0)
-    # u.setGoal(0.1, 0)
-    # u.setGoal(-0.1, 0)
     pid = PID(Ki = 0.5, Kp=2, Kd = 0.25)
     for i in range(10):
         back_signal = test_func(pid.output)
